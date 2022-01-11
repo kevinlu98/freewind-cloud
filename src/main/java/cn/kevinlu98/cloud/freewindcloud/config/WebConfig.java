fb45b741ce214db7707b1e3ab7f4a723a017af94
鲁恺文 <lukaiwen@xiaomi.com>
2022-01-11 20:48:24 +0800

    测试文件上传进度条

--- src/main/java/cn/kevinlu98/cloud/freewindcloud/config/WebConfig.java
+++ src/main/java/cn/kevinlu98/cloud/freewindcloud/config/WebConfig.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.util.ResourceUtils;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -28,6 +30,10 @@ import java.io.FileNotFoundException;
 public class WebConfig implements WebMvcConfigurer {
 
 
+    @Bean
+    public MultipartResolver multipartResolver(){
+        return new CommonsMultipartResolver();
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- src/main/java/cn/kevinlu98/cloud/freewindcloud/runner/DataInitRunner.java
+++ src/main/java/cn/kevinlu98/cloud/freewindcloud/runner/DataInitRunner.java
@@ -2,9 +2,12 @@ package cn.kevinlu98.cloud.freewindcloud.runner;
 
 import cn.kevinlu98.cloud.freewindcloud.common.Passwd;
 import cn.kevinlu98.cloud.freewindcloud.common.Website;
+import cn.kevinlu98.cloud.freewindcloud.common.enums.DriverType;
 import cn.kevinlu98.cloud.freewindcloud.common.enums.Role;
 import cn.kevinlu98.cloud.freewindcloud.mapper.UserMapper;
+import cn.kevinlu98.cloud.freewindcloud.pojo.Driver;
 import cn.kevinlu98.cloud.freewindcloud.pojo.User;
+import cn.kevinlu98.cloud.freewindcloud.service.DriverService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -22,6 +25,10 @@ import org.springframework.stereotype.Component;
 public class DataInitRunner implements ApplicationRunner {
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private DriverService driverService;
+
     @Autowired
     private Website website;
 
@@ -42,9 +49,15 @@ public class DataInitRunner implements ApplicationRunner {
                     .limitSize(website.uploadLimit())
                     .build();
             userMapper.save(user);
+
             log.info("以下是管理员信息：");
             log.info("用户名:{}", username);
             log.info("密码:{}", password);
+        }
+
+        Driver driver = driverService.activeDriver();
+        if (driver==null){
+            driverService.save(Driver.builder().name("默认空间").maxSize("50G").type(DriverType.DRIVER_LOCAL.getValue()).path("./driver").build());
         }
     }
 }
